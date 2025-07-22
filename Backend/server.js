e602a064a4820620255f048208bd008d2989c258
ankit-kumarpr <146826335+ankit-kumarpr@users.noreply.github.com>
2025-07-22 17:08:39 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 // Setup Socket.IO with proper CORS
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // frontend URL
+    origin: "https://chatnew-7ut1.onrender.com", // frontend URL
     methods: ["GET", "POST"],
     credentials: true,
   },
@@ -57,5 +57,5 @@ io.on("connection", (socket) => {
 });
 
 server.listen(port, () => {
-  console.log(`🚀 Backend running at http://localhost:${port}`);
+  console.log(`🚀 Backend running at ${port}`);
 });
